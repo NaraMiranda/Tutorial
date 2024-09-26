334157dae4a745fb1505a73dd4b8a4d8aee84a04
NaraMiranda <desenn@rpi.edu>
2024-09-26 16:55:17 -0400

    test script

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public int a;
+    public float b;
+    public Vector3 c;
+    public bool d;
+
+    public GameObject obj;
+    public Transform transform;
+
+    public Spinner spinner;
+    public Spawner spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
